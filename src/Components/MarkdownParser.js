ef8d063f8a71d9d909ba374af6b92302f6b73196
Pedram Mirshah <pedmsv@gamil.com>
2023-10-28 15:22:48 +0330

    fixed problem of not loading mathjax

--- src/Components/MarkdownParser.js
+++ src/Components/MarkdownParser.js
@@ -1,30 +1,31 @@
-  import React, { useState, useEffect } from "react";
-  import Markdown from "markdown-to-jsx";
-  import { useParams } from "react-router-dom";
-  import "../App.css";
-  import Layout from "../Components/Layout";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "../App.css";
+import Layout from "../Components/Layout";
 
-  const MarkdownParser = () => {
-    const [md, setMd] = useState("");
-    const [content, setContent] = useState("");
+const MarkdownParser = () => {
+  const [content, setContent] = useState("");
+  const { id } = useParams();
 
-    const { id } = useParams();
+  useEffect(() => {
+    import(`../content/${id}.html`).then((module) => {
+      setContent(module.default);
+    });
+  }, [id]);
 
-    useEffect(() => {
-      import(`../content/${id}.html`)
-        .then((module) => {
-          setContent(module.default);
-        });
-    }, [id]);
+  useEffect(() => {
+    if (window.MathJax) {
+      window.MathJax.Hub.Typeset();
+    }
+  }, [content]);
 
+  return (
+    <Layout>
+      <div>
+        <div className="post" dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
+    </Layout>
+  );
+};
 
-    return (
-      <Layout>
-        <div>
-          <div className="post" dangerouslySetInnerHTML={{ __html: content }} />
-        </div>
-      </Layout>
-    );
-  };
-
-  export default MarkdownParser;
+export default MarkdownParser;
